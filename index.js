c01a2a3f993f40b495d807ffed3751fa7f140e45
chriscotimms <chriscotimms@gmail.com>
2024-01-03 16:17:07 +0000

    seperated ai function from server file and cleaned up

--- index.js
+++ index.js
@@ -1,49 +1,28 @@
 // const server = require("./server.js");
 
-const { OpenAI } = require("openai");
 const express = require('express');
 const fetch = require('node-fetch');
 require("dotenv").config();
 const app = express();
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const {callOpenAi} = require("./openai")
 
 
-async function generateResponse(message) {
-  try {
-    const response = await openai.chat.completions.create({
-      messages: [
-        { role: "system", content: message !== undefined ? message : "Hello" },
-      ],
-      model: "gpt-3.5-turbo",
-      max_tokens: 150,
-      n: 1,
-    });
-
-    return response.choices[0].message.content;
-  } catch (error) {
-    console.error("OpenAI API Error:", error);
-    throw error;
-  }
-}
-
-
-const message = "what is white?";
+const message = "what is clarity?";
 
 app.get('/', async (req, res) => {
     // const message = req.body.message;
     // console.log("Received message from client:", message);
     
     try {
-    const openaiResponse = await generateResponse(message);
+    const openaiResponse = await callOpenAi(message);
     console.log(openaiResponse);
     res.send(openaiResponse);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
