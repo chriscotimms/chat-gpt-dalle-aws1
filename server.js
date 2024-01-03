766490114374c80ed694fa5d5737e85fef8615d8
chriscotimms <chriscotimms@gmail.com>
2024-01-03 11:25:23 +0000

    adding test and rudimentary elements from FAC example to establish main. To use for other branches from now on.

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (request, response) => {
+    response.send("hello");
+  });
+
+module.exports = server;
---
+++ test/test/server.test.js
@@ -0,0 +1,6 @@
+const test = require("node:test");
+const assert = require("node:assert");
+
+test("the test works", () => {
+  assert.equal(1, 1);
+});
